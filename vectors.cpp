fd1c31d3cd7283a418295eb11076c37736849ab2
Fred Young <fredgyoung@gmail.com>
2026-01-18 15:20:47 -0500

    Initial commit

---
+++ vectors.cpp
@@ -0,0 +1,31 @@
+//
+// Created by Fred on 1/18/2026.
+//
+#include <iostream>
+#include <ostream>
+#include <vector>
+
+using namespace std;
+
+void printVector(vector<int> vect) {
+
+    for (const int v : vect) {
+        cout << v << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+
+    vector<int> ages = {1, 2, 3};
+    printVector(ages);
+
+    ages.push_back(4);
+    printVector(ages);
+
+    ages.pop_back();
+    printVector(ages);
+
+
+    return 0;
+}
