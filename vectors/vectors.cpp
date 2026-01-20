fb8410131d4a802c7bb8d0d3e1199a9b6e5f3f83
Fred Young <fredgyoung@gmail.com>
2026-01-19 20:50:32 -0500

    added

--- vectors/vectors.cpp
+++ vectors/vectors.cpp
@@ -17,17 +17,21 @@ void printVector(const vector<int>& ages, const string& message) {
 
 int main() {
 
-    vector<int> ages = {1, 2, 3};
+    vector<int> ages = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     printVector(ages, "Ages");
 
-    ages.push_back(4);
+    ages.push_back(10);
     printVector(ages, "Ages");
 
     ages.pop_back();
     printVector(ages, "Ages");
 
-    cout << "Front: " << ages.front() << endl;
-    cout << "Back: " << ages.back() << endl;
+    cout << ".front(): " << ages.front() << endl;
+    cout << ".back(): " << ages.back() << endl;
+    cout << ".at(1): " << ages.at(1) << endl;
+    cout << ".size(): " << ages.size() << endl;
+    cout << ".empty(): " << ages.empty() << endl;
+    cout << ".capacity(): " << ages.capacity() << endl;
 
 
     return 0;
